e8bf44fc237def7f6e1225f0e3a677a47f1a529e
한승훈 <gksgns2003@gmail.com>
2019-01-04 17:44:41 +0900

    success backjoon 11053

--- algorithm_Hoons/algorithm_Hoons/BackJoon/Dynamic/11053.cpp
+++ algorithm_Hoons/algorithm_Hoons/BackJoon/Dynamic/11053.cpp
@@ -8,6 +8,7 @@ int main()
     int input;
     int *nums;
     int *dy;
+    int result=0;
 
     scanf("%d", &input);
 
@@ -20,11 +21,38 @@ int main()
     }
 
     dy[1] = 1;
+    int temp;
+
     for (int i = 2; i <= input; i++)
     {
-        
+        temp = 0;
+        for (int j = 1; j < i; j++)
+        {
+            if (nums[i] > nums[j])
+            {
+                if (temp < dy[j])
+                {
+                    temp = dy[j];
+                }
+            }
+        }
+        if (temp >= 1)
+            dy[i] = temp + 1;
+        else
+            dy[i] = 1;
     }
 
-    
+    for (int i = 1; i <= input; i++)
+    {
+        if (result < dy[i])
+        {
+            result = dy[i];
+        }
+    }
+    printf("%d", result);
+    //for (int i = 1; i <= input; i++)
+    //{
+    //    printf("%d / ", dy[i]);
+    //}
     return 0;
 }
