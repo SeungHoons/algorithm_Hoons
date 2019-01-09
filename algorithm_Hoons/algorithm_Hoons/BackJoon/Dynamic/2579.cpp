e1a6086e2deb70d7b1ebfe84046144dedac6f426
한승훈 <gksgns2003@gmail.com>
2019-01-09 17:31:50 +0900

    success backjoon 2579

--- algorithm_Hoons/algorithm_Hoons/BackJoon/Dynamic/2579.cpp
+++ algorithm_Hoons/algorithm_Hoons/BackJoon/Dynamic/2579.cpp
@@ -8,16 +8,21 @@ int main()
 {
     int input;
     int nums[301];
-    long long dy[301][3];
+    long long dy[301];
     scanf("%d", &input);
 
-    for (int i = 0; i < input; i++)
+    for (int i = 1; i <= input; i++)
     {
-        scanf("%d", nums[i]);
+        scanf("%d", &nums[i]);
+    }
+    
+    dy[1] = nums[1];
+    dy[2] = nums[1] + nums[2];
+
+    for (int i = 3; i <= input; i++)
+    {
+        dy[i] = max(dy[i - 2] + nums[i], dy[i - 3] + nums[i - 1] + nums[i]);
     }
 
-    for (int i = 0; i < input; i++)
-    {
-        dy[i+1][0] = max(nums[i+1],)
-    }
+    printf("%d", dy[input]);
 }
